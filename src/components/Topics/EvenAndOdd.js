5a0bf01c3adad30d3ac50ea5d7186c422787783e
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-01-31 17:42:04 -0600

    current-commit

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -33,14 +33,13 @@ assignEvenAndOdds(userInput){
 
     render(){
     return(
-        <div className = "puzzleBoxEvenAndOddPB">
-        <h4>"EvenAndOdds"</h4>
-        <input className = "inputLine"
-        onChange = { (e) => this.handleChange(e.target.value) }></input>
-        <button className = "confirmationButton" onClick = { () => { this.assignEvenAndOdds(this.state.userInput) } }>Split</button>
-        <span className = "resultsBox">Evens: {JSON.stringify(this.state.evenArray)}</span>
-        <span className = "resultsBox">Odds: {JSON.stringify(this.state.oddArray)}</span>
-        </div>
+        <div className="puzzleBox evenAndOddPB">
+        <h4> Evens and Odds </h4>
+        <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+        <button className="confirmationButton" onClick={ () => { this.assignEvenAndOdds(this.state.userInput) }}> Split </button>
+        <span className="resultsBox"> Evens: { JSON.stringify(this.state.evenArray) } </span>
+        <span className="resultsBox"> Odds: { JSON.stringify(this.state.oddArray) } </span>
+      </div>
         )
     } 
 }
